8691794639838364e29ab2cba0a0a1869a948d13
Sienna <148205111+SiennaAgg@users.noreply.github.com>
2024-01-10 02:31:26 +0000

    adding scrollable animation to the website

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,24 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+    // Code for scrolling animation from : https://alvarotrigo.com/blog/css-animations-scroll/
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
